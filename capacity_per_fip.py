e799b14dc5f15d85d695f5751570308ef2348121
Shankar Chawla <139474458+shankarchawla1776@users.noreply.github.com>
2023-12-16 20:26:57 -0700

    Update capacity_per_fip.py

--- capacity_per_fip.py
+++ capacity_per_fip.py
@@ -7,19 +7,24 @@ file_path = 'battery_storage.csv'
 df = pd.read_csv(file_path, delimiter='\t')
 fips_column = df['fips']
 # print(fips_column)
+capacity_columns = df['capacity']
+# print(capacity_columns)
 
 
 fips_array = fips_column.to_numpy()
 unique_fips = len(set(fips_array)) # => 88 
 
+average_capacity = df.groupby('fips')['capacity'].mean().sort_values(ascending=False)
 
 fips_counts = fips_column.value_counts()
 
-plt.bar(range(len(fips_counts)), fips_counts.values, width=0.25)
+plt.bar(range(len(average_capacity)), average_capacity.values, width=0.25) # y => fips_counts 
 
-for i, label in enumerate(fips_counts.values):
+
+
+for i, label in enumerate(average_capacity.values): # => fips_counts.values
     plt.annotate(
-        f'#{label}', #{fips_counts.index[i]}: #{label}
+        f'', # => {fips_counts.index[i]}: #{label} => Cap: {label:.2f}
         (i, label),
         xytext=(0, 5),
         textcoords='offset points',
@@ -30,15 +35,16 @@ for i, label in enumerate(fips_counts.values):
     )
 
 
-# Set x-axis labels to be the FIPS codes
-plt.xticks(range(len(fips_counts)), fips_counts.index, rotation=90)
+
+plt.xticks(range(len(average_capacity)), average_capacity.index, rotation=90) # => fips_counts, fips_counts.index
 plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=True)
 
 plt.xlabel('FIPS')
-plt.ylabel('Count')
+plt.ylabel('Average Capacity')
 plt.title('FIPS Counts')
 plt.yticks(rotation=90)
 
 
 mplcursors.cursor(hover=True)
+
 plt.show()
